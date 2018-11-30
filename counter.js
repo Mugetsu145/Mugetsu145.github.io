1d51b92ef50a5f510769885344a2ec4f47985c3a
Mugetsu145 <malachi.jones@harknessinstitute.com>
2018-11-30 03:59:57 -0600

    final version

--- counter.js
+++ counter.js
@@ -1,40 +1,80 @@
  var button = document.getElementById("button").value
  var counter = []
-//<img src="universe.png"  style="width:100%">
 
-function move(x) {
+
+
+/*function move(x) {
+var libro = document.getElementById("hola").value
 if(x.keyCode == 13){
-counter.push('hola')	
+ arcobaleno = String.fromCharCode(10)
+ saya = libro.split('p')
+}
+}
+document.onkeydown = move 
+*/
+
+
+/*var arcobaleno = String.fromCharCode(10)
+var duo 
+var libro = document.getElementById("hola").value
+for(duo; duo == 'hola' ; duo ){
+	alert('stil in')
+	function move(x){
+if(x.keyCode == 13){
+break
 }
 }
 document.onkeydown = move 
 
+}
+alert('out')
+*/
 
 
 function lol(){
-function chill(){
+	function chill(){
 var libro = document.getElementById("hola").value
 var yes = libro.split(' ')
-if(yes == ','){
-	alert("You have 0 words")
+var arcobaleno = String.fromCharCode(10)
+
+var saya = libro.split(arcobaleno)
+var helios = saya.filter(random =>{
+	return random == ''
+})
+var passed = yes.filter(espacio => {
+	return  espacio  == ''
+})
+alert(saya)
+if( yes == ','){
+alert('you have 0 words')
 }
-else{
-var yes = yes.length-1
-var yes = yes + counter.length
+else if(yes[0] == ''){ // if there is a space in the beggining this runs
+yes= yes.length - passed.length + saya.length - helios.length - 1
 alert("The length of this text is " + yes + ' words')
+}else{ // if there is no space in the beggining, this runs
+yes = yes.length - passed.length + saya.length - helios.length - 1
+alert("The length of this text is " + yes  + ' words')
 }
 }
 chill()
 }
 
 
-function whatever(){
+
+//write the enter key inside a loop, if another key that is not an enter key is
+//pressed, then you get out of the loop, but, only one enter is going to be validaded
+
+
+
+//The code below this, is code from before that is not used aymore. It iskeept around in case I need to use it again.
+
+/*function whatever(){
 var libro = document.getElementById("hola").value		
 counter.push(libro)
 counter.shift()
 counter.push(libro)
 	}
-
+*/
  
 
 
